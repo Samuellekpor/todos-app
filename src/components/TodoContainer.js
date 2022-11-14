0fa29aeb703e81acef721afbb57b66814ad546cd
Samuellekpor <samuellekpor@gmail.com>
2022-11-14 21:10:47 +0000

    Add style to the app

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -51,7 +51,7 @@ class TodoContainer extends React.Component {
 
   addTodoItem = title => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title: title,
       completed: false
     };
@@ -62,14 +62,16 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem}/>
-        <TodosList
-        todos={this.state.todos}
-        handleChangeProps={this.handleChange}
-        deleteTodoProps={this.delTodo}
-        />
+      <div className='container'>
+        <div className='inner'>
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem}/>
+          <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
