0e78de81facc1ab3d8187399c7f3b96fe00f9e5b
Samuellekpor <samuellekpor@gmail.com>
2022-11-15 10:36:06 +0000

    Add about page and handle with routers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import TodoContainer from './functionBased/components/TodoContainer';
 import './functionBased/App.css';
+import NotMatch from './functionBased/pages/NotMatch'
+import About from './functionBased/pages/About'
+import Navbar from './functionBased/components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
